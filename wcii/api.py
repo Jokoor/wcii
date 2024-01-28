adb30f5c7333647030ce26e9367923737a9b4169
WCII <wcii@wcii.gm>
2024-01-28 03:06:50 +0100

    nyass

--- wcii/api.py
+++ wcii/api.py
@@ -13,5 +13,13 @@ def overview():
     doc.db.set_value("total_courses", total_courses)
     doc.db.set_value("total_modules", total_modules)
 
-
-
+@frappe.whitelist()
+def get_class_students(class_name=""):
+    if not class_name or not frappe.db.exists('Class', class_name, cache=True): return []
+    
+    try:
+        classdoc=frappe.get_doc('Class', class_name)
+        
+        return [s for s in classdoc.students]
+    except: 
+        return []
