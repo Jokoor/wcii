a8d75906ef68031e055e98aca27cdb24e4e613b4
WCII <wcii@wcii.gm>
2024-02-17 14:54:46 +0100

    updates

--- wcii/wcii/doctype/e_books/e_books.py
+++ wcii/wcii/doctype/e_books/e_books.py
@@ -1,21 +1,29 @@
 # Copyright (c) 2024, Royalsmb and contributors
 # For license information, please see license.txt
 
+from urllib.request import urlopen
 import frappe
 from frappe.model.document import Document
-import urllib.request
-from pdf2image import convert_from_path
-from frappe.utils import get_site_path
+import requests
 class EBooks(Document):
 	def validate(self):
-		self.get_book_thumbnail()	
+		kwargs={
+			"thumbnail_link": self.thumbnail_link
+		}
+		self.get_book_thumbnail(**kwargs)	
 #download the file using urrlib
-	def get_book_thumbnail(self, ignore_permissions=True):
-			url = self.thumbnail_link
-			urllib.request.urlretrieve(url, f'/home/khan/frappe-bench/sites/app.wcii.royalsmb.com/public/files/{self.name}.png')
-			self.thumbnail_link = f'/files/{self.name}.png'
-			self.image = f'/files/{self.name}.png'
-			self.ignore_permissions=True
-
-			# image = convert_from_path(f'/home/khan/frappe-bench/sites/wcii/public/files/{self.name}.pdf')
-			# image[0].save(f'/home/khan/frappe-bench/sites/wcii/public/files/{self.name}.png', 'PNG')
+	def get_book_thumbnail(self, **kwargs):
+			data = frappe._dict(kwargs)
+			try:
+				f = urlopen(data.get("thumbnail_link"))
+				myfile = f.read()
+				_file  = frappe.get_doc({
+					"doctype": "File",
+					"file_name": self.file_name,
+					"is_private": 0,
+					"content": myfile
+				})
+			except Exception as e:
+				frappe.log_error(frappe.get_traceback())
+				
+			
--- wcii/wcii/doctype/student/student.py
+++ wcii/wcii/doctype/student/student.py
@@ -4,18 +4,22 @@
 import frappe
 from frappe.model.document import Document
 
+
 class Student(Document):
     def before_save(self):
-        fullname = f'{self.first_name or ""} {self.middle_name or ""} {self.last_name or ""}'
-        self.fullname=' '.join([i for i in fullname.split(' ') if i])
-        
+        fullname = (
+            f'{self.first_name or ""} {self.middle_name or ""} {self.last_name or ""}'
+        )
+        self.fullname = " ".join([i for i in fullname.split(" ") if i])
+
     def after_insert(self):
-         self.create_student_report()
-         students = frappe.db.count("Student")
-         overview = frappe.get_doc("Overview")
-         overview.total_students = students
-         overview.save()
-         frappe.db.commit()
+        self.create_student_report()
+        students = frappe.db.count("Student")
+        overview = frappe.get_doc("Overview")
+        overview.total_students = students
+        overview.save()
+        frappe.db.commit()
+
     def validate(self):
         # self.get_module()
         pass
@@ -25,18 +29,19 @@ class Student(Document):
 
         course_doc = frappe.get_doc("Course", self.course)
         for module in course_doc.module:
-            self.append("modules", {
-                "module": module.module,
-                "module_name": module.module_name
-            })
+            self.append(
+                "modules", {"module": module.module, "module_name": module.module_name}
+            )
 
-    #create student report doctype
+    # create student report doctype
     def create_student_report(self):
-        #create student report doctype
-        student_report = frappe.get_doc({
-            'doctype': 'Student Report',
-            'student': self.name,
-            'student_name': self.first_name
-        })
+        # create student report doctype
+        student_report = frappe.get_doc(
+            {
+                "doctype": "Student Report",
+                "student": self.name,
+                "student_name": self.first_name,
+            }
+        )
         student_report.insert()
         ignore_permissions = True
